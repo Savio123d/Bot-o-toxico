045627ed64025a5bc356dd5e4b7bd015cfd6a20f
Savio123d <savio13foxy@gmail.com>
2025-03-23 13:40:11 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let canMove = false;
+
+function activateMovement() {
+    canMove = true;
+    let buttonNo = document.getElementById('no');
+    buttonNo.textContent = "Agora tenta me pegar!";
+    buttonNo.onmouseover = moveButton;
+}
+
+function moveButton() {
+    if (!canMove) return;
+    let button = document.getElementById('no');
+    let x = Math.random() * (window.innerWidth - button.clientWidth);
+    let y = Math.random() * (window.innerHeight - button.clientHeight);
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+}
