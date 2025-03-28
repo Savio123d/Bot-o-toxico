66b840c4e6c2cf4e74a0f2d80f1ef2f2778f2f15
Savio <savio13foxy@gmail.com>
2025-03-28 17:57:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-let canMove = false;
-
-function activateMovement() {
-    canMove = true;
-    let buttonNo = document.getElementById('no');
-    buttonNo.textContent = "Agora tenta me pegar!";
-    buttonNo.onmouseover = moveButton;
-}
-
-function moveButton() {
-    let button = document.getElementById('no');
+function moveButton(){
+    let button = document.getElementById("nao");
     let x = Math.random() * (window.innerWidth - button.clientWidth);
     let y = Math.random() * (window.innerHeight - button.clientHeight);
-    button.style.left =  x + "px";
-    button.style.top = y + "px";
+
+    button.style.left = x + "px";
+    button.style.top = y  + "px";
 }
